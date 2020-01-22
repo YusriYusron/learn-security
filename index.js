70c892e637683ff206c1aa59e148b6443ce1bf0b
Yusri Yusron <yusriyusron0530@gmail.com>
2020-01-22 14:42:23 +0700

    Security for Brute Force Attack

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const rateLimit = require('express-rate-limit')
 const xss = require('xss-clean')
 const helmet = require('helmet')
+const brcypt = require('bcrypt') 
 
 const app = express()
 
@@ -20,3 +21,16 @@ const limit = rateLimit({
 app.use('/', limit) //Setup limiter on specific route
 
 app.use(xss()) //Clean XSS
+
+brcypt.hash('mypassword',10, (error, hash) => {
+     //Example print hash password
+     console.log(hash)
+     //Compare hash password to default
+     brcypt.compare('mypassword',hash, (error, result) => {
+         if (result) {
+            console.log('True') //Print true, if hash password and default password is same
+         } else {
+            console.log('False') //Print false
+         }
+     })
+})
