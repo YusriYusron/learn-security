ce84ad5713c507cdfda9b1dd31caf699e6ae43ce
Yusri Yusron <yusriyusron0530@gmail.com>
2020-01-22 14:22:54 +0700

    Another security for DOS Attack using express-rate-limit

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+const rateLimit = require('express-rate-limit')
+
 const app = express()
 
 app.use(express.json({
     limit: '10kb'
 }))
+
+const limit = rateLimit({
+    max: 100, //Maximum request
+    windowMs: 60 * 60 * 1000, // 1 hour banned/lockout
+    message: 'Too many request' //Message to send
+})
+
+app.use('/', limit) //Setup limiter on specific route
